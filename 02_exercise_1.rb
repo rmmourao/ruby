96da696055992fc96964cfaf3297605ce69cb62f
Rodrigo Mourao <rmmourao@gmail.com>
2020-01-20 16:58:21 -0300

    Created 02_exercise_1.rb and 03_exercise_2.rb

---
+++ 02_exercise_1.rb
@@ -0,0 +1,3 @@
+=begin
+O cachorro recebe a raça e o nome da inicialização. Não é permitido trocar a raça do cachorro depois de inicializado o objeto. O cachorro pode latir “au au!” por padrão ou personalizado através de parâmetro. Teste ao menos com 2 objetos.
+=end
---
+++ 03_exercise_2.rb
@@ -0,0 +1,4 @@
+=begin
+O papagaio é inicializado com nome e idade. Os dois podem ser alterados após a inicialização. O método repetir_frase deve usar um parâmetro com a frase a ser repetida. Caso não seja passado parâmetro o papagaio deve “dizer” “curupaco!”.
+Teste ao menos com 2 objetos.
+=end
